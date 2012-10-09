d1b8c446b238d7b4c33278299522464598c9b83d
spousty <spousty@redhat.com>
2012-10-08 18:51:54 -0700

    trying to figure things out

--- db.js
+++ db.js
@@ -13,41 +13,43 @@ exports.init = function() {
 
     //initialize the database
     db.open( function(err, db) {
-            if(!err){
-            console.log('>>DB open');
-            console.log('>>Dropping old addresses collection...');
-            
-            //testing
-            db.stats(function(err, items){
-            		console.log( items);
-            });
-            
-            // drop the collection if it exists
-            db.dropCollection('addresses', function(err, result){
-                console.log('>> Collection dropped');
-                });
-            // Get the collection set up, if not existing already
-            db.createCollection('addresses', {safe:true}, function(err, collection){
-
-
-                if(!err) {
-                //do something
-                console.log('>> Collection Created, DB ready for use');
-                collection.insert({name:'Freeman, Jonathan', phone:'+1.919.321.0119', address:'345 West Main St, Durham, NC 27701', email:'info@osintegrators.com'}, {safe:true}, function( err, result) {
-                if(err) {
-                    console.log(err);
-                }
-                else {
-                    console.log('fake doc added');
-                }
-                });
-                }
-                else {
-                    console.log('error');
-                    console.log(err);
-                }
-                });
-            }
+    		db.authenticate(dbUser, dbPass, function(err, db) {
+				if(!err){
+				console.log('>>DB open');
+				console.log('>>Dropping old addresses collection...');
+				
+				//testing
+				db.stats(function(err, items){
+						console.log( items);
+				});
+				
+				// drop the collection if it exists
+				db.dropCollection('addresses', function(err, result){
+					console.log('>> Collection dropped');
+					});
+				// Get the collection set up, if not existing already
+				db.createCollection('addresses', {safe:true}, function(err, collection){
+	
+	
+					if(!err) {
+					//do something
+					console.log('>> Collection Created, DB ready for use');
+					collection.insert({name:'Freeman, Jonathan', phone:'+1.919.321.0119', address:'345 West Main St, Durham, NC 27701', email:'info@osintegrators.com'}, {safe:true}, function( err, result) {
+					if(err) {
+						console.log(err);
+					}
+					else {
+						console.log('fake doc added');
+					}
+					});
+					}
+					else {
+						console.log('error');
+						console.log(err);
+					}
+					});
+				}
+			});
         db.close();
     });
 };
